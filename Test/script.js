08727ee237de89e5a4f5f104565eab1844e117bd
Alexandr <alex10_08@mail.ru>
2022-01-02 17:47:24 +0100

    выполнена 19ДЗ усложненка

--- Test/script.js
+++ Test/script.js
@@ -2,7 +2,6 @@ const input = document.getElementById('one')
 const p = document.querySelector('#result')
 let timeOut;
 input.addEventListener('input', (e) => {
-    console.log(e.target.value);
     clearTimeout(timeOut)
     timeOut = setTimeout(() => {
         p.textContent = e.target.value
