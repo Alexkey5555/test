597304c300ac8c34772c94d85bddf8c0ffcc488f
Alexandr <alex10_08@mail.ru>
2022-01-02 20:18:40 +0100

    выполнена усложненка 2

--- Test/script.js
+++ Test/script.js
@@ -1,10 +1,34 @@
-const input = document.getElementById('one')
-const p = document.querySelector('#result')
-let timeOut;
-input.addEventListener('input', (e) => {
-    clearTimeout(timeOut)
-    timeOut = setTimeout(() => {
-        p.textContent = e.target.value
-    }, 300)
+const div = document.getElementById('block')
+const btn = document.getElementById('btn')
+const reset = document.getElementById('reset')
 
+let interval
+let count = 0
+let active = false;
+
+const animate = () => {
+    count++
+    interval = requestAnimationFrame(animate)
+    if (count < 150) {
+        div.style.left = count * 2 + 'px'
+    }
+    else {
+        cancelAnimationFrame(interval)
+    }
+}
+btn.addEventListener('click', (e) => {
+    if (active) {
+        cancelAnimationFrame(interval)
+        active = false
+    }
+    else {
+        interval = requestAnimationFrame(animate)
+        active = true
+    }
+})
+reset.addEventListener('click', () => {
+    cancelAnimationFrame(interval)
+    div.style.left = 10 + 'px'
+    count = 0
+    active = false
 })
